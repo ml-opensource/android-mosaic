0335ef659f2245201731f36d296063c615de1325
Roman Levinzon <levinzon.roman@gmail.com>
2024-02-22 10:01:32 +0100

    feat: add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,22 @@
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.spotless)
+    alias(libs.plugins.detekt) apply false
+}
+
+
+subprojects {
+    apply(plugin = rootProject.libs.plugins.detekt.get().pluginId)
+    configure<DetektExtension> {
+        buildUponDefaultConfig = true
+        allRules = false
+        config.setFrom("$rootDir/detekt.yml")
+    }
 }
 
 spotless {
