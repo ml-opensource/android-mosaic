4235aa765da85cfd9d943419796bce6dec21c06b
Roman Levinzon <levinzon.roman@gmail.com>
2024-02-23 14:47:21 +0100

    fix(Slider): Add requiredSize for the SliderLayout

--- slider/src/main/java/io/monstarlab/mosaic/slider/SliderLayout.kt
+++ slider/src/main/java/io/monstarlab/mosaic/slider/SliderLayout.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.requiredSizeIn
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
@@ -13,16 +14,19 @@ import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.offset
 import kotlin.math.max
 import kotlin.math.roundToInt
 
 @Composable
-internal fun SliderLayout(
+public fun SliderLayout(
     progress: Float,
+    modifier: Modifier = Modifier,
     track: @Composable () -> Unit,
     thumb: @Composable () -> Unit
 ) {
     Layout(
+        modifier = modifier.requiredSizeIn(SliderDefaults.HandleWidth, SliderDefaults.HandleHeight),
         content = {
             Box(modifier = Modifier.layoutId(SliderLayoutElements.Track)) {
                 track()
@@ -32,22 +36,24 @@ internal fun SliderLayout(
             }
         }) { mesuarables, constraints ->
 
-        val trackPlaceable = mesuarables
-            .first { it.layoutId == SliderLayoutElements.Track }
-            .measure(constraints)
-
         val thumbPlaceable = mesuarables
             .first { it.layoutId == SliderLayoutElements.Thumb }
             .measure(constraints)
 
-        val sliderHeight = max(trackPlaceable.height, thumbPlaceable.height)
+        val trackPlaceable = mesuarables
+            .first { it.layoutId == SliderLayoutElements.Track }
+            .measure(constraints.offset(
+                horizontal = - thumbPlaceable.width
+            ).copy(minHeight = 0))
+
+
+        val sliderHeight = max(thumbPlaceable.height, trackPlaceable.height)
         val sliderWidth = trackPlaceable.width
         layout(sliderWidth, sliderHeight) {
-            val trackY = sliderHeight / 2 - trackPlaceable.height / 2
-            val thumbY = sliderHeight / 2 - thumbPlaceable.height / 2
-            val thumbX = (sliderWidth * progress).roundToInt() - thumbPlaceable.width / 2
-            trackPlaceable.place(0, trackY)
-            thumbPlaceable.place(thumbX, thumbY)
+            val thumbX = (trackPlaceable.width * progress).roundToInt() - thumbPlaceable.width / 2
+            val trackY = sliderHeight - trackPlaceable.height
+            trackPlaceable.placeRelative(0, trackY / 2)
+            thumbPlaceable.place(thumbX, 0)
         }
     }
 }
@@ -62,18 +68,21 @@ internal enum class SliderLayoutElements {
 @Composable
 private fun PreviewSliderLayout() {
     SliderLayout(
+        modifier = Modifier.fillMaxWidth(1f),
         progress = 0.5f,
         track = {
             Box(
                 modifier = Modifier
                     .background(Color.Yellow)
+                    .height(15.dp)
                     .fillMaxWidth()
-                    .height(20.dp)
             )
         },
         thumb = {
-            Box(modifier = Modifier
-                .background(Color.Blue, shape = CircleShape)
-                .size(50.dp))
+            Box(
+                modifier = Modifier
+                    .size(32.dp)
+                    .background(Color.Black, shape = CircleShape)
+            )
         })
 }
