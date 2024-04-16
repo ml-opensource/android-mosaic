28ed2791df450f9679dcf63e4a5d790512248ac7
kamel.mohamad <kamel.mohamad@monstar-lab.com>
2024-04-16 15:29:51 +0200

    create linear equation from 2 points

--- slider/src/main/java/io/monstarlab/mosaic/slider/LinearEquation.kt
+++ slider/src/main/java/io/monstarlab/mosaic/slider/LinearEquation.kt
@@ -7,4 +7,13 @@ public data class LinearEquation(
     public fun valueFromOffset(offset: Float): Float = m * offset + c
 
     public fun offsetFromValue(value: Float): Float = (value - c) / m
+
+   public companion object{
+       public fun fromTowPoints(x1:Float,y1:Float,x2:Float,y2:Float): LinearEquation {
+           require(x2!=x1){"can't calc equation from points with similar x value"}
+           val slope = (y2-y1)/(x2-x1)
+           val c = y2 - slope*x2
+           return  LinearEquation(slope,c)
+       }
+   }
 }
